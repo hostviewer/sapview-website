9c5551ecbc307b788af6eb1166cc4fc6d9ecdb21
hostviewer <103626638+hostviewer@users.noreply.github.com>
2022-04-28 10:43:44 -0700

    contact page update

--- js/script.js
+++ js/script.js
@@ -53,21 +53,3 @@ class BeforeAfter {
 }
 
 
-window.addEventListener("load", function() {
-    const form = document.getElementById('my-form');
-    form.addEventListener("submit", function(e) {
-      e.preventDefault();
-      const data = new FormData(form);
-      const action = e.target.action;
-      fetch(action, {
-        method: 'POST',
-        body: data,
-      })
-      .then(() => {
-        alert("Success!");
-      })
-    });
-  });
-  
-  
-
